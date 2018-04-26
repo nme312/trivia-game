74d132985b84a6ad64dd3ff8e1b2c24b330f5cc2
SkyNet <nme312.noreply.github.com>
2018-04-26 10:01:10 -0500

    created timers for trivia screen and reveal screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,73 +2,107 @@
 var triviaList = [
     {
         question: "What is the name of Ferris Bueller's girl friend?",
-        correct: "A.) Sloane Petterson",
-        incorrect: ["B.) Rose Wilson", " C.) Molly Henderson", " D.) Sydney Crange"],
+        answers: ["A.) Sloane Petterson", "B.) Rose Wilson", " C.) Molly Henderson", " D.) Sydney Crange"],
+        correct: 0,
     },
 
     {
         question: "How did Richard Kimble track down his wife's killer in The Fugitive 1993?",
-        correct: "A.) By tracing his prostetic arm",
-        incorrect: []
+        answers: ["A.) By tracing his prostetic arm",],
+        correct: 0,
     },
 
     {
         question: "What was the name of The Cheif of Police in the first Jaws film?",
-        correct: "A.) Martin Brody",
-        incorrect: []
+        answers: ["A.) Martin Brody",],
+        correct: 0,
     },
 
     {
         question: "In the first instalment of the movie franchise Scream, what does the killer talk to his second victim about?",
-        correct: "A.) Horror Movies",
-        incorrect: []
+        answers: ["A.) Horror Movies",],
+        correct: 0,
     },
 
     {
         question: "In Top Gun what is the codename of Maverick's copilot?",
-        correct: "A.) Goose",
-        incorrect: []
+        answers: ["A.) Goose",],
+        correct: 0,
     },
 
     {
         question: "In a famous scene from The GodFather, how does Don Corleone intimidate one of his enemies?",
-        correct: "A.) He has a severed horse head placed in his bed while he is sleeping",
-        incorrect: []
+        answers: ["A.) He has a severed horse head placed in his bed while he is sleeping",],
+        correct: 0,
     },
 
     {
         question: "What speed did Marty McFly have to reach in the DeLorean in order to activate the flux copacitor time machine in Back To The Future?",
-        correct: "A.) 88 mph",
-        incorrect: [" B.) Rose Wilson ", " C.) Molly Henderson", " D.) Sydney Crange"],
-    }
+        answers: ["A.) 88 mph", " B.) 60mph ", " C.) 100mph", " D.) 68mph"],
+        correct: 0,
+    },
 ]
 
-for (i = 0; i < triviaList.length; i++) {
-    console.log(triviaList[i].question);
-    console.log(triviaList[i].correct);
-    console.log(triviaList[i].incorrect);
-}
-
+//loop through trivia list array
+// for (i = 0; i < triviaList.length; i++) {
+//     console.log(triviaList[i].question);
+//     console.log(triviaList[i].answers);
+//     console.log(triviaList[i].correct);
+// }
 
 //create array to store user answers
-var userAnswers
-
-//variables for question screen and result screen timers
-var questionTime = 15;
-var resultTime = 7;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
 
 //variables for question screen and result screen inteval ids
 var questionInterval;
 var resultInterval;
-var gameTime
+var gameTime;
 
 //variables to keep track of questions and answers
 var currentQuestion;
 var currentAnswers;
 
-// loop through triviaList array
-function displayQuestion(x) {
-    $("#question").text(triviaList[x].question);
-    $("#answers").append(triviaList[x].answers);
-    $("#answers").append(triviaList[x].incorrect);
+//create game timer and reveal timer
+questionInterval = setInterval(triviaScreen, 1000);
+
+//screen functions to run game and reveal screens
+var x = 0;
+function triviaScreen() {
+
+    console.log(x);
+    console.log(triviaList[x].question);
+    console.log(triviaList[x].answers);
+    console.log(triviaList[x].answers[triviaList[x].correct]);
+    x++;
+    if(x === triviaList.length) {
+        clearInterval(questionInterval);
+    }
+
+    revealInterval = setTimeout(revealScreen, 5000);
+
+    function revealScreen() {
+        console.log(triviaList[x].answers[triviaList[x].correct]);
+    }
 }
+
+//gain access to buttons and divs
+var questionDiv = $("#question");
+var answersDiv = $("#answers");
+
+var radioA = $("#answer-a");
+var radioB = $("#answer-b");
+var radioC = $("#answer-c");
+var radioD = $("#answer-d");
+
+// console.log(radioA.val());
+// console.log(radioB.val());
+// console.log(radioC.val());
+// console.log(radioD.val());
+
+//manipulate buttons with jq
+// radioA.val("it changed");
+// console.log(radioA.val());
+
+//function triggered on click event used to respond to correct and incorrect answers
